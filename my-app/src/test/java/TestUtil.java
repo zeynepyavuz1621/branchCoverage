368657e8c26af302531ee23cc13448db8c443d2e
zeynepyavuz1621 <zeynepyavuz1621@gmail.com>
2024-03-30 02:51:11 +0300

    The initial folder added.

---
+++ my-app/src/test/java/TestUtil.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUtil {
+  Util c;
+
+  @Before
+  public void setUp() { c = new Util(); }
+
+  @Test
+  public void example() { assertTrue(true); }
+
+}
